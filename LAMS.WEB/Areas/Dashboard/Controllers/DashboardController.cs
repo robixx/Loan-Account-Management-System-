fe9994fa61393625ae2208cf57e7c769c20c4e07
Shoriful Islam Robi <robi@itcbd.com>
2025-09-11 15:29:07 +0600

    LAMS

---
+++ LAMS.WEB/Areas/Dashboard/Controllers/DashboardController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LAMS.WEB.Areas.Dashboard.Controllers
+{
+    [Area("Dashboard")]
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ LAMS.WEB/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LAMS.WEB.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
