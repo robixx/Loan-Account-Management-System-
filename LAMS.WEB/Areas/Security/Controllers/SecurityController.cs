2cf070800b7361057ef20150f54897e39e1f5a10
Shoriful Islam Robi <robi@itcbd.com>
2025-09-15 12:15:39 +0600

    LAMS Profile Create

---
+++ LAMS.WEB/Areas/Security/Controllers/SecurityController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LAMS.WEB.Areas.Security.Controllers
+{
+    [Area("Security")]
+    public class SecurityController : Controller
+    {
+
+
+
+        [HttpGet]
+        public IActionResult CustomerProfile()
+        {
+            return View();
+        }
+    }
+}
